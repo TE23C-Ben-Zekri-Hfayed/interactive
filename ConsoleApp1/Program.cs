683017b3258c3402a4b315d536ae1a657cdc30e2
Hfayed Ben Zekri <hfayed.benzekri@elev.ga.ntig.se>
2024-09-13 09:39:01 +0200

    tränat på CSS och Box modelle, margin, padding, border

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,12 @@
 ﻿Console.WriteLine("Username:");
 String name = Console.ReadLine();
 
-
-
+if (name == "freaky")
+{
+  Console.WriteLine("Welcome Freaky");
+}
+else
+{
+    Console.WriteLine("Invalid user");
+}
 Console.ReadLine();
